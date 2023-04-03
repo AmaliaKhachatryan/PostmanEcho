cf91428ef278eb877edcf264840c7cc9d304041c
Amalia Khachatryan <amalia-khachatryan@mail.ru>
2023-04-03 17:47:34 +0300

    bug fix

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -15,7 +15,7 @@ class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Hello"));
+                .body("data", equalTo("Hello world"));
     }
 
     @Test
